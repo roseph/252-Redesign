32d5fea745692a6c78771bb45e518084a484e2de
superhornet142 <roseph148@gmail.com>
2014-07-11 02:04:36 -0400

    Added Java Example

---
+++ Addition.java
@@ -0,0 +1,17 @@
+/*This program is a simple function that demonstrates
+  the structuring of a non-scripting programming language
+  such as Java
+*/ 
+public class Addition  {
+   public static void main(String[] args)  {
+      int x = 1;  //Set 1 into a variable
+      int y = 2;  //Set 2 into a variable
+      int z;      //Create a variable for the answer
+      
+      z = x + y;  //Perform the arithmetic
+      
+      System.out.print(z);  //Output the answer
+   }
+}
+      
+   
