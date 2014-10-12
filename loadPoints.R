de3b5ca6173f8e3adcef1befc8f44028f7a11f42
superhornet142 <roseph148@gmail.com>
2014-10-12 10:46:24 -0400

    Added loadPoints.R

---
+++ loadPoints.R
@@ -0,0 +1,18 @@
+readSpreadsheet <- function(url, sheet = 1){
+   library(httr)
+   r <- GET(url)
+   html <- content(r)
+   sheets <- readHTMLTable(html, header=FALSE, stringsAsFactors=FALSE)
+   df <- sheets[[sheet]]
+   dfClean <- function(df){
+     nms <- t(df[1,])
+     names(df) <- nms
+     df <- df[-1,-1] 
+     row.names(df) <- seq(1,nrow(df))
+     //df[,5] <- as.numeric(as.character(df[,5]))
+     df
+   }
+   dfClean(df)
+ }
+
+
